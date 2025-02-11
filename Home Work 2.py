9b7eb19e249c6d8246093d909ade92cdcf9fcba7
hello test <laskovoebal@gmail.com>
2025-02-12 01:04:40 +0200

    Home work 2.1

---
+++ Home Work 2.py
@@ -0,0 +1,11 @@
+user_input = input('Please, enter you number:') #это будет 4444
+print(user_input)
+number = int(user_input)
+print(number//1111)
+print(number//1111 * 4)
+print(number//1111 * 4 * 2)
+print(number//1111 * 4 * 2 // 4)
+
+
+
+
