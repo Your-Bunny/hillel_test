2348d2c748a6b00754d55c12be5ab41bb9219cbc
hello test <laskovoebal@gmail.com>
2025-02-21 16:38:19 +0200

    Home work 3.3

---
+++ Home Work 3.3.py
@@ -0,0 +1,14 @@
+lst = [1,2,3,4,5,6]
+count = len(lst)
+if count % 2 == 0:
+    print([lst[:count // 2],lst[count // 2:]])
+elif count % 2 == 1:
+    print([lst[:count // 2 + 1], lst[count // 2 + 1:]])
+
+
+
+
+
+
+
+
